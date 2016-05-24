3a53a1f1c4f711b34d16c9e5c03b61b6c029d2dc
shaozilee <shaozilee@gmail.com>
2016-05-24 16:26:07 +0800

    add upload func

--- src/main/java/com/shaozilee/dada/controller/AttachmentController.java
+++ src/main/java/com/shaozilee/dada/controller/AttachmentController.java
@@ -1,14 +1,19 @@
 package com.shaozilee.dada.controller;
 
 import com.shaozilee.dada.utils.AjaxCode;
+import com.shaozilee.dada.utils.Config;
+import com.shaozilee.dada.utils.Uploader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by lee on 15-9-13.
@@ -20,7 +25,30 @@ public class AttachmentController extends AbstractController{
 
     @RequestMapping("/doUpload")
     public void doUpload(HttpServletRequest request,HttpServletResponse response,Model model) throws Exception{
-        String jsonStr = toJson(AjaxCode.SUC, response);
+        String savePath = Config.get("attachment.path");
+        Uploader up = new Uploader(request);
+        up.setSavePath(savePath);
+        up.setMaxSize(2000); //单位KB
+        up.upload();
+
+        HashMap map = new HashMap();
+        map.put("code",AjaxCode.SUC);
+        map.put("name",up.getFileName());
+        map.put("originalName",up.getOriginalName());
+        map.put("size",up.getSize());
+        map.put("state",up.getState());
+        map.put("type",up.getType());
+        map.put("url",up.getUrl());
+
+        JSONObject resp = new JSONObject(map);
+        String jsonStr = resp.toString();
+        try{
+            response.getWriter().write(jsonStr);
+            response.getWriter().close();
+        }catch (IOException e){
+            logger.error(e.getMessage());
+        }
+
         logger.debug(jsonStr);
     }
 
